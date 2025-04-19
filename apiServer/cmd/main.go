e39d2648244e3d565e1f6c4c888b2485d3a3c9e2
WhitePeach <Elysia_awa@outlook.com>
2025-04-19 11:30:17 +0800

    ✨ feat(main): add version handler for processing version information requests

--- apiServer/cmd/main.go
+++ apiServer/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/MarsRH/PeachDoss/apiServer/heartbeat"
 	"github.com/MarsRH/PeachDoss/apiServer/locate"
 	"github.com/MarsRH/PeachDoss/apiServer/objects"
+	"github.com/MarsRH/PeachDoss/apiServer/version"
 	"github.com/MarsRH/PeachDoss/sys"
 )
 
@@ -18,12 +19,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	// 监听数据服务节点心跳
 	go heartbeat.ListenHeartbeat()
 	// 处理对象请求，实际上是将对象请求转发给数据服务
 	http.HandleFunc("/handleObjs/", objects.Handler)
 	// 处理定位请求
 	http.HandleFunc("/locateObj/", locate.Handler)
+	// 处理版本信息
+	http.HandleFunc("/versions/", version.Handler)
 	// 启动并监听服务
 	http.ListenAndServe(os.Getenv(sys.ListenAddress), nil)
 }
